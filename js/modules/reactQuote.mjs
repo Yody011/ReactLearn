e10321ebd520dc076f45de797571442b78932c4e
Yody <3162498907@qq.com>
2022-05-27 09:16:35 +0800

    增加React学习进度。学习scss超集。关于js相关知识巩固。做了个轮播图Demo

---
+++ js/modules/reactQuote.mjs
@@ -0,0 +1,10 @@
+/*
+ * @Author: Yody 3162498907@qq.com
+ * @Date: 2022-05-23 21:45:29
+ * @LastEditors: Yody
+ * @LastEditTime: 2022-05-23 22:02:50
+ * @Description: 模块化react引用。要引用react时就引用这个，如果引用位置要改，只需要改这里就好了
+ * 
+ */
+import '../library/react-17.0.2/react.production.min.js'
+import '../library/react-17.0.2/react-dom.production.min.js'
